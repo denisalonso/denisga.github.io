faa7e6b7250383e3f79bb45f75901b24c7f084f8
MarianaRosa13 <166611799+MarianaRosa13@users.noreply.github.com>
2024-11-18 18:17:05 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    enviar = document.querySelector('#enviar');
+    enviar.addEventListener('click', function () {
+        mensagem = document.querySelector('.mensagem-input').value;
+        li_clonado = document.querySelector('.env');
+        ol = document.querySelector('ol');
+        li_novo = li_clonado.cloneNode(true);
 
+        p_do_li_novo = li_novo.querySelector(".texto-mensagem");
+        p_do_li_novo.innerHTML = mensagem;
+        ol.appendChild(li_novo);
+
+        document.querySelector('.mensagem-input').value = "";   
+    })
+})
